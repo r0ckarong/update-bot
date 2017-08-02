70ac833e5303ddaa46e1e73da72a1b80a45a1674
Markus Napp <mnapp@arago.de>
2017-08-02 10:01:16 +0200

    Revert "Split update routines to separate modules"
    This reverts commit fabb584dd5e21b40021680acd226865c68e8ee4a.

--- update_bot.py
+++ update_bot.py
@@ -7,10 +7,6 @@ import os
 import pdb
 import json
 
-import find_g870a_update
-import find_qbt_update
-import find_kpxc_update
-
 known = ''
 known_versions = {}
 message = ''
@@ -58,7 +54,85 @@ def send_bot_msg(message):
     bot = telepot.Bot(os.environ['BOT_TOKEN'])
     bot.sendMessage(user_id, message)
 
+def find_g870a_update():
+    tp = 'g870a'
+    get_current(tp)
 
+    headers = {'Accept': 'application/json'}
+
+    r = requests.get('https://services.att.com/kmservices/v2/contents/KM1126238?app-id=esupport', headers=headers)
+
+    data = r.json()
+
+    xml = data['resultBody']['contentTypeProperties']['currentsoftdetails']
+    # xml2 = data['resultBody']['contentTypeProperties']['currentsoftupd']
+
+    # soup = BeautifulSoup(xml,'html.parser')
+
+    # Current version
+    pos = xml.find("Baseband version:",0)
+    verpos = xml.find("G870A",pos+17,pos+57)
+    version = xml[verpos:verpos+13]
+
+    # Previous version
+    prev = xml.rfind("Previous versions required:",0)
+    prev_ver = xml.find("G870A",prev+27)
+    previous_version = xml[prev_ver:prev_ver+13]
+
+    url =  'https://xdmd.sl.attcompute.com/agents/42998/1488/SS-' + previous_version + '-to-' + version[7:] + '-UP'
+
+    prepare_msg(tp, version, url)
+
+    if version != known:
+        send_bot_msg(message)
+        print(url)
+        append_known(tp, version)
+    else:
+        print('No new version for G870A')
+
+def find_qbt_update():
+    tp = 'qbt'
+    get_current(tp)
+
+    html = requests.get('https://www.qbittorrent.org/news.php')
+    soup = BeautifulSoup(html.text,'html.parser')
+    verstring = str(soup.p.string)
+
+    endpos = verstring.find(' was',0)
+    verpos = verstring.find('3', 0)
+    version = str(verstring[verpos-1:endpos])
+    url = 'https://sourceforge.net/projects/qbittorrent/files/qbittorrent/qbittorrent-' + version[1:] + '/qbittorrent-' + version[1:] + '.tar.gz/download'
+
+    prepare_msg(tp, version, url)
+
+    if version != known:
+        send_bot_msg(message)
+        print(url)
+        append_known(tp, version)
+    else:
+        print('No new version for qBittorrent')
+
+def find_kpxc_update():
+    tp = 'kpxc'
+    get_current(tp)
+
+    html = requests.get('https://keepassxc.org/blog/feed.xml')
+    soup = BeautifulSoup(html.text,'html.parser')
+    verstring = str(soup.item.title)
+
+    verpos = verstring.find('KeePassXC ', 0)
+    endpos = verstring.find(' released',0)
+    version = str(verstring[verpos+10:endpos])
+    url = str(soup.item.guid.string)
+
+    prepare_msg(tp, version, url)
+
+    if version != known:
+        send_bot_msg(message)
+        print(url)
+        append_known(tp, version)
+    else:
+        print('No new version for KeePassXC')
 
 schedule.every().day.at("00:00").do(do_updates)
 schedule.every().day.at("12:00").do(do_updates)
