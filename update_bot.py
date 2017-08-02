c9e7e3e6d2995baa91438d0bf22c4dc762b7a918
Markus Napp <mnapp@arago.de>
2017-08-02 12:05:31 +0200

    Refactor bot functions

--- update_bot.py
+++ update_bot.py
@@ -10,39 +10,43 @@ import json
 known = ''
 known_versions = {}
 message = ''
+version = ''
+data = ''
 
-def do_updates():
-    find_g870a_update()
-    find_qbt_update()
-    find_kpxc_update()
-
-def get_current(tp):
+def get_current(package):
     global known_versions
     global known
     with open('known_versions.json','r') as verfile:
         known_versions = json.load(verfile)
-        known = str(known_versions[tp][-1])
+        known = str(known_versions[package][-1])
     return known
 
-def append_known(tp, version):
+def strain_version(verstring, verpos, endpos):
+    global version
+    start = verstring.find(verpos, 0)
+    end = verstring.find(endpos, 0)
+    sievelen = len(verpos)
+    version = str(verstring[start+sievelen:end])
+    return version
+
+def append_known(package, version):
     global known_versions
 
-    if version not in known_versions[tp]:
-        known_versions[tp].append(unicode(version))
+    if version not in known_versions[package]:
+        known_versions[package].append(unicode(version))
         with open('known_versions.json','w') as verfile:
             verlist = json.dumps(known_versions, indent=4, sort_keys=True)
             verfile.write(verlist)
     else:
-        print("Version " + version + " already known.")
+        print(package + 'version ' + version + ' already known.')
 
-def prepare_msg(tp, version, url):
+def liquidize(html):
+    global data
+    data = BeautifulSoup(html.text,'html.parser')
+    return data
+
+def prepare_msg(package, version, url):
     global message
-    if tp == 'g870a':
-        package = 'the G870A'
-    elif tp == 'qbt':
-        package = 'qBittorrent'
-    elif tp == 'kpxc':
-        package = 'KeePassXC'
 
     text = 'A new update for %s is released:\n%s\nDownload here: %s'
 
@@ -54,20 +58,25 @@ def send_bot_msg(message):
     bot = telepot.Bot(os.environ['BOT_TOKEN'])
     bot.sendMessage(user_id, message)
 
+def notify(package, version, url):
+    if version != known:
+        prepare_msg(package, version, url)
+        send_bot_msg(message)
+        print(url)
+        append_known(package, version)
+    else:
+        print('No new version for ' + package)
+
 def find_g870a_update():
-    tp = 'g870a'
-    get_current(tp)
+    package = 'G870A'
+    get_current(package)
 
-    headers = {'Accept': 'application/json'}
-
-    r = requests.get('https://services.att.com/kmservices/v2/contents/KM1126238?app-id=esupport', headers=headers)
-
-    data = r.json()
+    data = requests.get('https://services.att.com/kmservices/v2/contents/KM1126238?app-id=esupport', headers={'Accept': 'application/json'}).json()
 
     xml = data['resultBody']['contentTypeProperties']['currentsoftdetails']
     # xml2 = data['resultBody']['contentTypeProperties']['currentsoftupd']
 
-    # soup = BeautifulSoup(xml,'html.parser')
+    # data = BeautifulSoup(xml,'html.parser')
 
     # Current version
     pos = xml.find("Baseband version:",0)
@@ -81,58 +90,40 @@ def find_g870a_update():
 
     url =  'https://xdmd.sl.attcompute.com/agents/42998/1488/SS-' + previous_version + '-to-' + version[7:] + '-UP'
 
-    prepare_msg(tp, version, url)
-
-    if version != known:
-        send_bot_msg(message)
-        print(url)
-        append_known(tp, version)
-    else:
-        print('No new version for G870A')
+    notify(package, version, url)
 
 def find_qbt_update():
-    tp = 'qbt'
-    get_current(tp)
+    package = 'qBittorrent'
+    get_current(package)
 
     html = requests.get('https://www.qbittorrent.org/news.php')
-    soup = BeautifulSoup(html.text,'html.parser')
-    verstring = str(soup.p.string)
+    liquidize(html)
+    verstring = str(data.p.string)
 
-    endpos = verstring.find(' was',0)
-    verpos = verstring.find('3', 0)
-    version = str(verstring[verpos-1:endpos])
-    url = 'https://sourceforge.net/projects/qbittorrent/files/qbittorrent/qbittorrent-' + version[1:] + '/qbittorrent-' + version[1:] + '.tar.gz/download'
+    strain_version(verstring, 'qBittorrent ', ' was')
 
-    prepare_msg(tp, version, url)
+    url = 'https://sourceforge.net/projects/qbittorrent/files/qbittorrent/qbittorrent-' + version[0:] + '/qbittorrent-' + version[0:] + '.tar.gz/download'
 
-    if version != known:
-        send_bot_msg(message)
-        print(url)
-        append_known(tp, version)
-    else:
-        print('No new version for qBittorrent')
+    notify(package, version, url)
 
 def find_kpxc_update():
-    tp = 'kpxc'
-    get_current(tp)
+    package = 'KeePassXC'
+    get_current(package)
 
     html = requests.get('https://keepassxc.org/blog/feed.xml')
-    soup = BeautifulSoup(html.text,'html.parser')
-    verstring = str(soup.item.title)
+    liquidize(html)
+    verstring = str(data.item.title)
 
-    verpos = verstring.find('KeePassXC ', 0)
-    endpos = verstring.find(' released',0)
-    version = str(verstring[verpos+10:endpos])
-    url = str(soup.item.guid.string)
+    strain_version(verstring, 'KeePassXC ', ' released')
 
-    prepare_msg(tp, version, url)
+    url = str(data.item.guid.string)
 
-    if version != known:
-        send_bot_msg(message)
-        print(url)
-        append_known(tp, version)
-    else:
-        print('No new version for KeePassXC')
+    notify(package, version, url)
+
+def do_updates():
+    find_g870a_update()
+    find_qbt_update()
+    find_kpxc_update()
 
 schedule.every().day.at("00:00").do(do_updates)
 schedule.every().day.at("12:00").do(do_updates)
